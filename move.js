b85a3032fcda15392ec16f169f30c7e5944921b0
bluefantasy728 <jiangguoxi728@hotmail.com>
2016-10-11 08:45:26 +0800

    new create

---
+++ move.js
@@ -0,0 +1,242 @@
+function getStyle(obj,attr){
+	return (obj.currentStyle || getComputedStyle(obj,false))[attr];
+}
+//同时运动的代码
+function startMove(obj,json,optional){ //attr变成json，那里面的值都要变成json,attr和iTarget合并成json
+	
+	//运动的时间和回调函数都是可选的参数，所以将它们作为一个json传入{duration:--,complete:--}
+	optional = optional || {}; //当这个optional的json如果不穿，函数里会建立
+	optional.duration = optional.duration || 500; //没有设定duration时，会将它默认设置成500毫秒
+	optional.complete = optional.complete || null; //如果没有回调函数，就将它设置为空对象null
+	optional.easing = optional.easing || 'ease-out'; //默认是减速运动
+	clearInterval(obj.timer);
+	var start = {};
+	var dis = {};
+	for (var key in json){
+		start[key] = parseFloat(getStyle(obj,key));
+		if(isNaN(start[key])){
+			switch (key){
+				case 'width':
+				start[key] = obj.offsetWidth;
+					break;
+				case 'height':
+				start[key] = obj.offsetHeight;
+					break;
+				case 'opacity':
+				start[key] = 1;
+					break;
+				case 'left':
+				start[key] = 0;
+					break;
+				case 'top':
+				start[key] = 0;
+					break;
+				//marginLeft/top...	padding
+			}
+		}
+		dis[key] = json[key] - start[key];
+	}
+	var n = 0;
+	var count = Math.round( optional.duration /30);
+	obj.timer = setInterval(function(){
+		n++;
+		for(var key in json){	
+			switch(optional.easing){
+				case 'linear':
+				var iCur = start[key] + n/count*dis[key];
+					break;
+				case 'ease-in':
+				var a = n/count;
+				var iCur = start[key] + Math.pow(a,5)*dis[key]; //加速运动
+					break;
+				case 'ease-out':
+				var a = 1 - n/count;
+				var iCur = start[key] + (1-Math.pow(a,5))*dis[key]; //减速运动
+					break;
+			}
+			if(key == 'opacity'){
+				obj.style.opacity = iCur;
+				obj.style.filter = 'alpha(opacity:'+ iCur*100 +')';
+			}else{
+				obj.style[key] = iCur + 'px';
+			}
+		}
+		if(n == count){
+			clearInterval(obj.timer);
+			optional.complete && optional.complete();
+		}
+	},30);
+}
+function startMove2(obj,json,optional){
+	optional = optional || {};
+	optional.duration = optional.duration || 500;
+	optional.complete = optional.complete || null;
+	optional.easing = optional.easing || 'ease-out';
+	
+	var start = {};
+	var dis = {};
+	
+	for (var key in json) {
+		start[key] = parseFloat(getStyle(obj,key));
+		if(isNaN(start[key])){
+			switch (key){
+				case 'height':
+				start[key] = obj.offsetHeight;
+				break;
+				case 'width':
+				start[key] = obj.offsetWidth;
+				break;
+				case 'top':
+				start[key] = 0;
+				break;
+				case 'opacity':
+				start[key] = 1;
+				break;
+			}
+		}
+		dis[key] = json[key] - start[key];
+	}
+	var n = 0;
+	var count = Math.round(optional.duration/30);
+	
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		n++;
+		for(var key in json){
+			switch (optional.easing){
+				case 'linear':
+				var iCur = start[key] + n/count*dis[key];
+					break;
+				case 'ease-in':
+				var a = n/count;
+				iCur = start[key] + Math.pow(a,5)*dis[key];
+					break;
+				case 'ease-out':
+				var a = 1-n/count;
+				iCur = start[key] + (1 - Math.pow(a,5))*dis[key];
+					break;
+			}
+			
+			if(key == 'opacity'){
+				obj.style.opacity = iCur;
+				obj.style.filter = 'alpha(opacity:'+ iCur*100 +')';
+			}else{
+				obj.style[key] = iCur + 'px';
+			}
+			if(n == count){
+				clearInterval(obj.timer);
+				optional.complete && optional.complete();
+			}
+		}
+	},30);
+}
+
+function startMove3(obj,json,optional){
+	optional = optional || {};
+	optional.duration = optional.duration || 500;
+	optional.complete = optional.complete || null;
+	optional.easing = optional.easing || 'ease-out';
+	
+	var start = {};
+	var dis = {};
+	
+	for(var key in json){
+		start[key] = parseFloat(getStyle(obj,key));
+		if (isNaN(start[key])){
+			switch (key){
+				case 'opacity':
+				start[key] = 1;
+					break;
+				case 'top':
+				start[key] = 0;
+					break;
+				case 'left':
+				start[key] = 0;
+					break;
+			}
+		}
+		dis[key] = json[key] - start[key];
+	}
+	var n = 0;
+	var count = Math.round(optional.duration / 30);
+	
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		n++;
+		switch (optional.easing){
+			case 'linear':
+			var iCur = start[key] + n/count*dis[key];
+				break;
+			case 'ease-in':
+			var a = n/count;
+			var iCur = start[key] + a*a*a*dis[key];
+				break;
+			case 'ease-out':
+			var iCur = n/count*dis[key];
+				break;
+		}
+	},30);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ myfunction.js
@@ -0,0 +1,140 @@
+function findInArray(n,arr){
+	for (var i=0; i<arr.length; i++) {
+		if(arr[i] == n){
+			return true;
+		}
+	}
+	return false;
+}
+
+function setStyle(){
+	if(arguments.length == 3){
+		arguments[0].style[arguments[1]] = arguments[2];
+	}else if(arguments.length == 2){
+		for(var name in arguments[1]){
+			arguments[0].style[name] = arguments[1][name];
+		}
+	}else{
+		alert('请输入正确的参数');
+	}
+}
+
+function getStyle(obj,attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	}else{
+		return getComputedStyle(obj,false)[attr];
+	}
+}
+
+function findMinIndex(arr,index){ //返回数组中从index算起最小值的索引值
+	var min = arr[index];
+	var ind = index;
+	for (var i=index; i<arr.length; i++) {
+		if(arr[i]<min){
+			min = arr[i];
+			ind = i;
+		}
+	}
+	return ind;
+}
+
+function rnd(m,n){
+	return parseInt(Math.random()*(n-m+1)+m);
+}
+
+function toDou(n){
+	return n<10?'0'+n:''+n;
+}
+//通过class获取元素
+function getByClass(obj,classname){
+	if(obj.getElementsByClassName){
+		return obj.getElementsByClassName(classname);
+	}else{
+		var aEl = obj.getElementsByTagName('*');
+		var newArr = [];
+		for (var i=0; i<aEl.length; i++) {
+			var arr = aEl[i].className.split(' ');
+			for (var j=0; j<arr.length; j++) {
+				if(arr[j] == classname){
+					newArr.push(aEl[i]);
+				}
+			}
+		}
+		return newArr;
+	}
+}
+//添加事件
+function addEvent(obj,eventname,fn){
+	if(obj.addEventListener){
+		obj.addEventListener(eventname,fn,false);
+	}else{
+		obj.attachEvent('on'+eventname,function(){
+			fn.call(obj);
+		});
+	}
+}
+//解绑事件
+function removeEvent(obj,eventname,fn){
+	if(obj.removeEventListener){
+		obj.removeEventListener(eventname,fn,false);
+	}else{
+		obj.detachEvent('on'+eventname,fn);
+	}
+}
+//碰撞函数
+function testColl(obj1,obj2){
+	var l1 = obj1.offsetLeft;
+	var r1 = l1 + obj1.offsetWidth;
+	var t1 = obj1.offsetTop;
+	var b1 = t1 + obj1.offsetHeight;
+	var l2 = obj2.offsetLeft;
+	var r2 = l2 + obj2.offsetWidth;
+	var t2 = obj2.offsetTop;
+	var b2 = t2 + obj2.offsetHeight;
+	
+	if(l1>r2 || r1<l2 || t1>b2 || b1<t2){
+		return false;
+	}else{
+		return true;
+	}
+}
+//获取2个块元素的中心点间的距离
+function getDis(obj1,obj2){
+	var a = obj2.offsetLeft - obj1.offsetLeft;
+	var b = obj2.offsetTop - obj1.offsetTop;
+	return Math.sqrt(a*a+b*b);
+}
+//将aA中index最大的那个排除，其他的都放到一个新数组中，这么做是因为，新添加的a标签在aA中肯定排在后面，为了一直能选中带加号的a，就一直让它的index是最大的
+function getArrayByIndex(aA){
+	var arr = [];
+	for(var i=0; i<aA.length; i++){
+		if(aA[i].index != aA.length-1){
+			arr.push(aA[i]);
+		}
+	}
+	return arr;
+}
+//查看obj的是否包含classname的类
+function findHasClass(obj,classname){
+	var arr = obj.className.split(' ');
+	for (var i=0; i<arr.length; i++) {
+		if(arr[i] == classname){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
